3de3de009ae5ed9535fa3b3a68e9cb49a0a796a8
Sdegraauw <stijn.degraauw@student.fontys.nl>
2026-02-09 15:24:20 +0100

    endpoint setup

---
+++ API/AI quizelet API/AI quizelet API/Controllers/Class.cs
@@ -0,0 +1,30 @@
+﻿using AI_quizelet_API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AI_quizelet_API.Controllers
+{
+    [ApiController]
+    [Route("insta")]
+    public class InstagramControler
+    {
+        InstagramControler() { }
+
+        [HttpGet]
+        public Post GetPost()
+        {
+            //TODO retrieve IGpost from DB
+
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public bool Postanswer(string userName, int postId)
+        {
+            //TODO validate answer
+
+            //TODO save answer to DB
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API/AI quizelet API/AI quizelet API/DTOs/Post.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace AI_quizelet_API.DTOs
+{
+    public class Post
+    {
+        public int id { get; set; }
+        public string discription { get; set; }
+        public BitArray BitArray { get; set; }
+
+        public Post(int id, string discription, BitArray bitArray)
+        {
+            this.id = id;
+            this.discription = discription;
+            this.BitArray = bitArray;
+        }
+    }
+}
